3234a737c50b7b4dc2c321af15d88d3b0d66d3a2
Peter T Bosse II <ptb@ioutime.com>
2017-09-23 16:53:35 -0400

    Update Radarr to 0.2.0.852

--- Casks/radarr.rb
+++ Casks/radarr.rb
@@ -1,6 +1,6 @@
 cask 'radarr' do
-  version '0.2.0.846'
-  sha256 'af765a5081b3cd40e51cd538d89cec2f06450ce4e185f71c3e6a19493589167f'
+  version '0.2.0.852'
+  sha256 'f00282767fbc64b6e0d337f3a20662097c8c042ca52a4e1d32367895cbde3e5e'
 
   # github.com/Radarr/Radarr was verified as official when first introduced to the cask
   url "https://github.com/Radarr/Radarr/releases/download/v#{version}/Radarr.develop.#{version}.osx-app.zip"
